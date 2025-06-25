693c95c278e15ab769f776cac08c585c2f1f88aa
Dawam Raja <akunnyadawam9@gmail.com>
2025-06-25 10:43:27 +0700

    Remove 'required' attribute from various input fields in MKL creation form to allow optional input.

--- resources/views/admin/mkl/create.blade.php
+++ resources/views/admin/mkl/create.blade.php
@@ -26,49 +26,49 @@
                 </div>
                 <div class="form-group">
                     <label for="nama_mkl">Nama MKL</label>
-                    <input type="text" class="form-control @error('nama_mkl') is-invalid @enderror" id="nama_mkl" name="nama_mkl" value="{{ old('nama_mkl') }}" required>
+                    <input type="text" class="form-control @error('nama_mkl') is-invalid @enderror" id="nama_mkl" name="nama_mkl" value="{{ old('nama_mkl') }}" >
                     @error('nama_mkl')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="nama_pt_mkl">Nama PT MKL</label>
-                    <input type="text" class="form-control @error('nama_pt_mkl') is-invalid @enderror" id="nama_pt_mkl" name="nama_pt_mkl" value="{{ old('nama_pt_mkl') }}" required>
+                    <input type="text" class="form-control @error('nama_pt_mkl') is-invalid @enderror" id="nama_pt_mkl" name="nama_pt_mkl" value="{{ old('nama_pt_mkl') }}" >
                     @error('nama_pt_mkl')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="no_telepon_pribadi">No. Telepon Pribadi</label>
-                    <input type="text" class="form-control @error('no_telepon_pribadi') is-invalid @enderror" id="no_telepon_pribadi" name="no_telepon_pribadi" value="{{ old('no_telepon_pribadi') }}" required>
+                    <input type="text" class="form-control @error('no_telepon_pribadi') is-invalid @enderror" id="no_telepon_pribadi" name="no_telepon_pribadi" value="{{ old('no_telepon_pribadi') }}" >
                     @error('no_telepon_pribadi')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="no_telepon_kantor">No. Telepon Kantor</label>
-                    <input type="text" class="form-control @error('no_telepon_kantor') is-invalid @enderror" id="no_telepon_kantor" name="no_telepon_kantor" value="{{ old('no_telepon_kantor') }}" required>
+                    <input type="text" class="form-control @error('no_telepon_kantor') is-invalid @enderror" id="no_telepon_kantor" name="no_telepon_kantor" value="{{ old('no_telepon_kantor') }}" >
                     @error('no_telepon_kantor')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="email_kantor">Email Kantor</label>
-                    <input type="email" class="form-control @error('email_kantor') is-invalid @enderror" id="email_kantor" name="email_kantor" value="{{ old('email_kantor') }}" required>
+                    <input type="email" class="form-control @error('email_kantor') is-invalid @enderror" id="email_kantor" name="email_kantor" value="{{ old('email_kantor') }}" >
                     @error('email_kantor')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="npwp_kantor">NPWP Kantor</label>
-                    <input type="text" class="form-control @error('npwp_kantor') is-invalid @enderror" id="npwp_kantor" name="npwp_kantor" value="{{ old('npwp_kantor') }}" required>
+                    <input type="text" class="form-control @error('npwp_kantor') is-invalid @enderror" id="npwp_kantor" name="npwp_kantor" value="{{ old('npwp_kantor') }}" >
                     @error('npwp_kantor')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="menggunakan_mtki_payment">Menggunakan MTKI Payment</label>
-                    <select class="form-control @error('menggunakan_mtki_payment') is-invalid @enderror" id="menggunakan_mtki_payment" name="menggunakan_mtki_payment" required>
+                    <select class="form-control @error('menggunakan_mtki_payment') is-invalid @enderror" id="menggunakan_mtki_payment" name="menggunakan_mtki_payment" >
                         <option value="1" {{ old('menggunakan_mtki_payment') == '1' ? 'selected' : '' }}>Ya</option>
                         <option value="0" {{ old('menggunakan_mtki_payment') == '0' ? 'selected' : '' }}>Tidak</option>
                     </select>
@@ -85,7 +85,7 @@
                 </div>
                 <div class="form-group">
                     <label for="status_aktif">Status Aktif</label>
-                    <select class="form-control @error('status_aktif') is-invalid @enderror" id="status_aktif" name="status_aktif" required>
+                    <select class="form-control @error('status_aktif') is-invalid @enderror" id="status_aktif" name="status_aktif" >
                         <option value="1" {{ old('status_aktif') == '1' ? 'selected' : '' }}>Aktif</option>
                         <option value="0" {{ old('status_aktif') == '0' ? 'selected' : '' }}>Tidak Aktif</option>
                     </select>
@@ -95,14 +95,14 @@
                 </div>
                 <div class="form-group">
                     <label for="file_ktp">File KTP</label>
-                    <input type="file" class="form-control @error('file_ktp') is-invalid @enderror" id="file_ktp" name="file_ktp" required>
+                    <input type="file" class="form-control @error('file_ktp') is-invalid @enderror" id="file_ktp" name="file_ktp" >
                     @error('file_ktp')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="file_npwp">File NPWP</label>
-                    <input type="file" class="form-control @error('file_npwp') is-invalid @enderror" id="file_npwp" name="file_npwp" required>
+                    <input type="file" class="form-control @error('file_npwp') is-invalid @enderror" id="file_npwp" name="file_npwp" >
                     @error('file_npwp')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -117,10 +117,10 @@
             $('#menggunakan_mtki_payment').change(function() {
                 if ($(this).val() == '0') {
                     $('#alasan_container').show();
-                    $('#alasan_tidak_menggunakan_mtki_payment').prop('required', true);
+                    $('#alasan_tidak_menggunakan_mtki_payment').prop('', true);
                 } else {
                     $('#alasan_container').hide();
-                    $('#alasan_tidak_menggunakan_mtki_payment').prop('required', false);
+                    $('#alasan_tidak_menggunakan_mtki_payment').prop('', false);
                 }
             }).trigger('change');
         });
