1f8f51c1b60f03c1114b3e2488cd98d9f61fac66
Dawam Raja <akunnyadawam9@gmail.com>
2025-06-25 14:36:05 +0700

    update

--- resources/views/admin/mkl/edit.blade.php
+++ resources/views/admin/mkl/edit.blade.php
@@ -17,56 +17,56 @@
                 </div>
                 <div class="form-group">
                     <label for="nama_pribadi">Nama Pribadi</label>
-                    <input type="text" class="form-control @error('nama_pribadi') is-invalid @enderror" id="nama_pribadi" name="nama_pribadi" value="{{ old('nama_pribadi', $mkl->nama_pribadi) }}" required>
+                    <input type="text" class="form-control @error('nama_pribadi') is-invalid @enderror" id="nama_pribadi" name="nama_pribadi" value="{{ old('nama_pribadi', $mkl->nama_pribadi) }}" >
                     @error('nama_pribadi')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="nama_mkl">Nama MKL</label>
-                    <input type="text" class="form-control @error('nama_mkl') is-invalid @enderror" id="nama_mkl" name="nama_mkl" value="{{ old('nama_mkl', $mkl->nama_mkl) }}" required>
+                    <input type="text" class="form-control @error('nama_mkl') is-invalid @enderror" id="nama_mkl" name="nama_mkl" value="{{ old('nama_mkl', $mkl->nama_mkl) }}" >
                     @error('nama_mkl')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="nama_pt_mkl">Nama PT MKL</label>
-                    <input type="text" class="form-control @error('nama_pt_mkl') is-invalid @enderror" id="nama_pt_mkl" name="nama_pt_mkl" value="{{ old('nama_pt_mkl', $mkl->nama_pt_mkl) }}" required>
+                    <input type="text" class="form-control @error('nama_pt_mkl') is-invalid @enderror" id="nama_pt_mkl" name="nama_pt_mkl" value="{{ old('nama_pt_mkl', $mkl->nama_pt_mkl) }}" >
                     @error('nama_pt_mkl')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="no_telepon_pribadi">No. Telepon Pribadi</label>
-                    <input type="text" class="form-control @error('no_telepon_pribadi') is-invalid @enderror" id="no_telepon_pribadi" name="no_telepon_pribadi" value="{{ old('no_telepon_pribadi', $mkl->no_telepon_pribadi) }}" required>
+                    <input type="text" class="form-control @error('no_telepon_pribadi') is-invalid @enderror" id="no_telepon_pribadi" name="no_telepon_pribadi" value="{{ old('no_telepon_pribadi', $mkl->no_telepon_pribadi) }}" >
                     @error('no_telepon_pribadi')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="no_telepon_kantor">No. Telepon Kantor</label>
-                    <input type="text" class="form-control @error('no_telepon_kantor') is-invalid @enderror" id="no_telepon_kantor" name="no_telepon_kantor" value="{{ old('no_telepon_kantor', $mkl->no_telepon_kantor) }}" required>
+                    <input type="text" class="form-control @error('no_telepon_kantor') is-invalid @enderror" id="no_telepon_kantor" name="no_telepon_kantor" value="{{ old('no_telepon_kantor', $mkl->no_telepon_kantor) }}" >
                     @error('no_telepon_kantor')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="email_kantor">Email Kantor</label>
-                    <input type="email" class="form-control @error('email_kantor') is-invalid @enderror" id="email_kantor" name="email_kantor" value="{{ old('email_kantor', $mkl->email_kantor) }}" required>
+                    <input type="email" class="form-control @error('email_kantor') is-invalid @enderror" id="email_kantor" name="email_kantor" value="{{ old('email_kantor', $mkl->email_kantor) }}" >
                     @error('email_kantor')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="npwp_kantor">NPWP Kantor</label>
-                    <input type="text" class="form-control @error('npwp_kantor') is-invalid @enderror" id="npwp_kantor" name="npwp_kantor" value="{{ old('npwp_kantor', $mkl->npwp_kantor) }}" required>
+                    <input type="text" class="form-control @error('npwp_kantor') is-invalid @enderror" id="npwp_kantor" name="npwp_kantor" value="{{ old('npwp_kantor', $mkl->npwp_kantor) }}" >
                     @error('npwp_kantor')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="menggunakan_mtki_payment">Menggunakan MTKI Payment</label>
-                    <select class="form-control @error('menggunakan_mtki_payment') is-invalid @enderror" id="menggunakan_mtki_payment" name="menggunakan_mtki_payment" required>
+                    <select class="form-control @error('menggunakan_mtki_payment') is-invalid @enderror" id="menggunakan_mtki_payment" name="menggunakan_mtki_payment" >
                         <option value="1" {{ old('menggunakan_mtki_payment', $mkl->menggunakan_mtki_payment) == '1' ? 'selected' : '' }}>Ya</option>
                         <option value="0" {{ old('menggunakan_mtki_payment', $mkl->menggunakan_mtki_payment) == '0' ? 'selected' : '' }}>Tidak</option>
                     </select>
@@ -83,7 +83,7 @@
                 </div>
                 <div class="form-group">
                     <label for="status_aktif">Status Aktif</label>
-                    <select class="form-control @error('status_aktif') is-invalid @enderror" id="status_aktif" name="status_aktif" required>
+                    <select class="form-control @error('status_aktif') is-invalid @enderror" id="status_aktif" name="status_aktif" >
                         <option value="1" {{ old('status_aktif', $mkl->status_aktif) == '1' ? 'selected' : '' }}>Aktif</option>
                         <option value="0" {{ old('status_aktif', $mkl->status_aktif) == '0' ? 'selected' : '' }}>Tidak Aktif</option>
                     </select>
@@ -125,10 +125,10 @@
             $('#menggunakan_mtki_payment').change(function() {
                 if ($(this).val() == '0') {
                     $('#alasan_container').show();
-                    $('#alasan_tidak_menggunakan_mtki_payment').prop('required', true);
+                    $('#alasan_tidak_menggunakan_mtki_payment').prop('', true);
                 } else {
                     $('#alasan_container').hide();
-                    $('#alasan_tidak_menggunakan_mtki_payment').prop('required', false);
+                    $('#alasan_tidak_menggunakan_mtki_payment').prop('', false);
                 }
             }).trigger('change');
         });
